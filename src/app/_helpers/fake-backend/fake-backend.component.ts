b25833f21502be9d9e002f1ce41e119ede77c4dc
garima28713 <garima28713@gmail.com>
2023-07-09 17:53:33 +0530

    jwt assignment 13

---
+++ src/app/_helpers/fake-backend/fake-backend.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-fake-backend',
+  templateUrl: './fake-backend.component.html',
+  styleUrls: ['./fake-backend.component.css']
+})
+export class FakeBackendComponent {
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { FakeBackendComponent } from './_helpers/fake-backend/fake-backend.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    FakeBackendComponent
   ],
   imports: [
     BrowserModule
